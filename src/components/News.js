7649070cb8df09d9f4baed0563ecbb9680832b65
Harshit <harshitjindal02@gmail.com>
2024-01-03 22:15:42 +0530

    Refactored the code

--- src/components/News.js
+++ src/components/News.js
@@ -17,14 +17,19 @@ export class News extends Component {
     category: PropTypes.string.isRequired
   }
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1
     }
+    document.title = `NewsMonkey - ${this.capitalizeFirstLetter(this.props.category)}`
   }
+
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
   async updateNews() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b91d583c777c4179a07bfc6492350e8d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
@@ -55,11 +60,11 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h1 className='text-center' style={{margin: "23px 0px"}}>NewsMonkey - Top Headlines</h1>
+        <h1 className='text-center' style={{margin: "23px 0px"}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner/>}
         <div className="row" >
           {!this.state.loading && this.state.articles.map((element)=> {
-            return <div className="col-md-3 my-1" key={element.url}>
+            return <div className="col-md-4 my-1" key={element.url}>
               <NewsItem title={element.title ? element.title.slice(0, 45) + "..." : ""} 
               description={element.description ? element.description.slice(0, 60) + "..." : ""} 
               imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source = {element.source.name} />
